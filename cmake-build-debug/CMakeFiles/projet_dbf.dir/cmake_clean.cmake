1a020aec4e2b0cf0a3f5cd057a6fe3423d3d44dd
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-01-31 15:22:01 +0100

    init clion project

---
+++ cmake-build-debug/CMakeFiles/projet_dbf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_dbf.dir/main.cpp.o"
+  "projet_dbf.pdb"
+  "projet_dbf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet_dbf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
