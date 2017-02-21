be309fd56aa64509341f8396d570508c65bdbc9b
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-02-21 15:47:18 +0100

    nettoyage du git

---
+++ codeDBF/DBFsrc/errno_dbf.h
@@ -0,0 +1,15 @@
+/**
+ * Class where you can undestand our own errno
+ */
+
+#ifndef PROJET_DBF_ERRNO_DBF_H
+#define PROJET_DBF_ERRNO_DBF_H
+
+#define ERRNO_DBF_OK 0
+
+//Error linked to socket
+#define ERRNO_DBF_ERROR_SOCKET_CLOSED -1
+#define ERRNO_DBF_ERROR_TO_READ_MESSAGE -2
+#define ERRNO_DBF_ERROR_TO_SEND_MESSAGE -3
+
+#endif //PROJET_DBF_ERRNO_DBF_H
---
+++ codeDBF/cmake-build-debug/CMakeFiles/projet_dbf.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/baptiste/Cours/projet_dbf/DBFsrc/SocketDbf.cpp" "/home/baptiste/Cours/projet_dbf/cmake-build-debug/CMakeFiles/projet_dbf.dir/DBFsrc/SocketDbf.cpp.o"
+  "/home/baptiste/Cours/projet_dbf/DBFsrc/SocketFactory.cpp" "/home/baptiste/Cours/projet_dbf/cmake-build-debug/CMakeFiles/projet_dbf.dir/DBFsrc/SocketFactory.cpp.o"
+  "/home/baptiste/Cours/projet_dbf/DBFsrc/main.cpp" "/home/baptiste/Cours/projet_dbf/cmake-build-debug/CMakeFiles/projet_dbf.dir/DBFsrc/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
