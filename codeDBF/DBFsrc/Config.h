352907c7572914e1b2a6711908a32aa709f5b4ae
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-03-20 15:08:55 +0100

    ajout d'un fichier qui servira plus tard à faire la conf, pour le moment c'est un simple fichier qui stocke des constantes

--- codeDBF/DBFsrc/Config.h
+++ codeDBF/DBFsrc/Config.h
@@ -6,3 +6,15 @@
 #define PROJET_DBF_CONFIG_H
 
 #endif //PROJET_DBF_CONFIG_H
+
+
+#include <iostream>
+#define addressBdd "127.0.0.1"
+
+class Config {
+public:
+
+    static const int portBdd = 3306;
+    static const int portDBF = 3305;
+
+};
