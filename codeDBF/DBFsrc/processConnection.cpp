61c7aff711a4372b8cf53de63bf4049c6e4461f9
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-03-20 15:45:03 +0100

    ajout de la lib thread au cmakelist

---
+++ codeDBF/DBFsrc/processConnection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by baptiste on 20/03/17.
+//
+
+#include "processConnection.h"
---
+++ codeDBF/DBFsrc/processConnection.h
@@ -0,0 +1,16 @@
+//
+// Created by baptiste on 20/03/17.
+//
+
+#ifndef PROJET_DBF_PROCESSCONNECTION_H
+#define PROJET_DBF_PROCESSCONNECTION_H
+
+
+
+class processConnection {
+
+};
+
+
+
+#endif //PROJET_DBF_PROCESSCONNECTION_H
