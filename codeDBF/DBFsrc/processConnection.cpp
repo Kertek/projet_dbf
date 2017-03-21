dc8af447b47c44b4238750d7742a2b2ee0356d16
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-03-21 15:33:49 +0100

    use of new prototype method to send and receive message

--- codeDBF/DBFsrc/processConnection.cpp
+++ codeDBF/DBFsrc/processConnection.cpp
@@ -9,46 +9,38 @@ processConnection::processConnection(Connection *connection) : mConnection(conne
 
 void processConnection::operator()() {
     cout << "premier echange: Récupération bdd + renvoi application" << endl;
-    vector<char> msg;
-    msg.resize(0, 0);
-    msg.clear();
-    int result = this->mConnection->getSocketBdd()->receiveMessage(msg);
 
-    this->mConnection->getSocketApplication()->sendMessage(&msg);
+    int result = this->mConnection->getSocketBdd()->receiveMessage(this->mConnection->getMessage());
+
+    this->mConnection->getSocketApplication()->sendMessage(this->mConnection->getMessage());
 
     cout << "deuxieme echange" << endl;
-    msg.resize(0, 0);
-    msg.clear();
 
-    this->mConnection->getSocketApplication()->receiveMessage(msg);
 
-    this->mConnection->getSocketBdd()->sendMessage(&msg);
+    this->mConnection->getSocketApplication()->receiveMessage(this->mConnection->getMessage());
+
+    this->mConnection->getSocketBdd()->sendMessage(this->mConnection->getMessage());
 
     cout << "troisième échange" << endl;
-    msg.resize(0, 0);
-    msg.clear();
 
-    result = this->mConnection->getSocketBdd()->receiveMessage(msg);
+    result = this->mConnection->getSocketBdd()->receiveMessage(this->mConnection->getMessage());
 
-    this->mConnection->getSocketApplication()->sendMessage(&msg);
+    this->mConnection->getSocketApplication()->sendMessage(this->mConnection->getMessage());
     cout << "quatrième echange" << endl;
-    msg.resize(0, 0);
-    msg.clear();
 
-    this->mConnection->getSocketApplication()->receiveMessage(msg);
 
-    this->mConnection->getSocketBdd()->sendMessage(&msg);
+    this->mConnection->getSocketApplication()->receiveMessage(this->mConnection->getMessage());
+
+    this->mConnection->getSocketBdd()->sendMessage(this->mConnection->getMessage());
 
     cout << "cinquième échange" << endl;
-    msg.resize(0, 0);
-    msg.clear();
-    result = this->mConnection->getSocketBdd()->receiveMessage(msg);
-    this->mConnection->getSocketApplication()->sendMessage(&msg);
+
+    result = this->mConnection->getSocketBdd()->receiveMessage(this->mConnection->getMessage());
+    this->mConnection->getSocketApplication()->sendMessage(this->mConnection->getMessage());
     cout << "sizième echange" << endl;
-    msg.resize(0, 0);
-    msg.clear();
-    this->mConnection->getSocketApplication()->receiveMessage(msg);
-    this->mConnection->getSocketBdd()->sendMessage(&msg);
+
+    this->mConnection->getSocketApplication()->receiveMessage(this->mConnection->getMessage());
+    this->mConnection->getSocketBdd()->sendMessage(this->mConnection->getMessage());
 
     this->mConnection->~Connection();
 
