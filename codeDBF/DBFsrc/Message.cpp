8e5cffb19b546746a3c4b142de59b57d82a2353a
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-03-22 10:51:29 +0100

    clean of debugging element and add of Handshake_Packet

--- codeDBF/DBFsrc/Message.cpp
+++ codeDBF/DBFsrc/Message.cpp
@@ -31,22 +31,25 @@ void Message::initMessage() {
 
 void Message::determineTypeMessage(unsigned int packetLength, unsigned int sequenceid, unsigned int packetHeader) {
     //TODO
-    cout << "packetHeader " << packetHeader << "packetLength" << packetLength << endl;
     if (packetHeader == 0x00 && packetLength >= 7) {
         this->mTypeMessage = TypeMessage::OK_Packet;
-        cout << "paquet de type OK_Packet" <<endl;
+        cout << "paquet de type OK_Packet" << endl;
         return;
     } else if (packetHeader == 0xfe && packetLength <= 9) {
         this->mTypeMessage = TypeMessage::EOF_Packet;
-        cout << "paquet de type EOF_Packet" <<endl;
+        cout << "paquet de type EOF_Packet" << endl;
         return;
     } else if (packetHeader == 0xff) {
         this->mTypeMessage = TypeMessage::ERR_Packet;
-        cout << "paquet de type ERR_Packet" <<endl;
+        cout << "paquet de type ERR_Packet" << endl;
+        return;
+    } else if (packetHeader == 0x0a){
+        this->mTypeMessage = TypeMessage ::Handshake_Packet;
+        cout << "paquet de type Handshake_Packet - Server Greeting" <<endl;
         return;
     } else {
         this->mTypeMessage = TypeMessage::Other;
-        cout << "paquet de type Other" <<endl;
+        cout << "paquet de type Other" << endl;
         return;
     }
 
