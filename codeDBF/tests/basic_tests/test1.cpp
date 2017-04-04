80e91736e78bb15c1ec738317e8518223ccefbcb
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-04-04 16:49:46 +0200

    lutte avec le système de tests

--- codeDBF/tests/basic_tests/test1.cpp
+++ codeDBF/tests/basic_tests/test1.cpp
@@ -3,7 +3,16 @@
 //
 
 #include "gtest/gtest.h"
+#include "../../DBFsrc/Message.h"
+#include "../../DBFsrc/Parser/calc_driver.h"
 
 TEST( test1 , testFonction ){
-    EXPECT_EQ(1, 0);
+    Message * m = new Message();
+    ASSERT_STREQ(m->getContent()->data(), nullptr);
+}
+
+TEST(test2,testParse){
+    CALC::CALC_Driver driver;
+    driver.parse("SELECT");
+    EXPECT_EQ(0,0);
 }
