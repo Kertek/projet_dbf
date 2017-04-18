687e12b6342c3a553a096f9d7d48733a82128209
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-04-18 12:49:21 +0200

    rename of tests

---
+++ codeDBF/DBFtest/TestsUnitaires/test1.cpp
@@ -0,0 +1,22 @@
+//
+// Created by menguy on 03/04/17.
+//
+
+#include "gtest/gtest.h"
+#include "../../DBFsrc/Parser/calc_driver.h"
+#include "../../DBFsrc/Message.h"
+
+
+TEST( test1 , testFonction ){
+    Message * m = new Message();
+    ASSERT_STREQ(m->getContent()->data(), nullptr);
+
+}
+
+TEST(test2,testParse){
+
+    CALC::CALC_Driver driver;
+    driver.parse("SELECT lala FROM toto");
+
+    EXPECT_EQ(0,0);
+}
