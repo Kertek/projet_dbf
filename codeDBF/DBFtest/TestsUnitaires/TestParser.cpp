c51fff2ead85d68423cb713b1d746b5e5d99ea13
Kertek <baptiste.polve@telecom-sudparis.eu>
2017-04-18 15:21:32 +0200

    resolu testparser

--- codeDBF/DBFtest/TestsUnitaires/TestParser.cpp
+++ codeDBF/DBFtest/TestsUnitaires/TestParser.cpp
@@ -4,18 +4,116 @@
 
 #include "gtest/gtest.h"
 #include "../../DBFsrc/Parser/calc_driver.h"
-#include "../../DBFsrc/Message.h"
+/*
+################
+# Doit marcher #
+################
+*/
 
-
-TEST( test1 , testFonction ){
-    Message * m = new Message();
-    ASSERT_STREQ(m->getContent()->data(), nullptr);
-
+TEST( TestParserSimpleSelect , simplePositiveSelect1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("SELECT 'coucou'"));
 }
 
-TEST(test2,testParse){
 
+TEST( TestParserSimpleSelect , simplePositiveSelect2){
     CALC::CALC_Driver driver;
-    ASSERT_TRUE(driver.parse("SELECT lala FROM toto"));
+    EXPECT_TRUE(driver.parse("select 'coucou'"));
+}
 
+TEST( TestParserSimpleSelectFrom , simplePositiveSelectFrom1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles"));
+}
+
+TEST( TestParserSimpleSelectFrom , simplePositiveSelectFrom2){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("SELECT id FROM Articles"));
+}
+
+TEST( TestParserSelectAsFrom , PositiveSelectAsFrom1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id as i from Articles"));
+}
+
+TEST( TestParserSelectAsFrom , PositiveSelectAsFrom2){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id as 'i' from Articles"));
+}
+
+TEST( TestParserSelectAsFrom , PositiveSelectAsFrom3){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("SELECT id AS i FROM Articles"));
+}
+
+TEST( TestParserSelectAsFrom , PositiveSelectAsFrom4){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id as i,date from Articles"));
+}
+
+TEST( TestParserSelectFromAs , PositiveSelectFromAs1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles as a"));
+}
+
+TEST( TestParserSelectFromWhere , PositiveSelectFromWhere1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where id=1"));
+}
+
+TEST( TestParserSelectFromWhere , PositiveSelectFromWhere2){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where id='1'"));
+}
+
+TEST( TestParserSelectFromWhere , PositiveSelectFromWhere3){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where 1=1"));
+}
+
+TEST( TestParserSelectFromWhere , PositiveSelectFromWhere4){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where 'l'='l'"));
+}
+
+TEST( TestParserComplexRequest , PositiveComplexRequest1){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select (select id from Articles where id=1) from Articles"));
+}
+
+TEST( TestParserComplexRequest , PositiveComplexRequest2){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select (select id from Articles where id=1) as l from Articles"));
+}
+
+TEST( TestParserComplexRequest , PositiveComplexRequest3){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from (select * from Articles) as l"));
+}
+
+TEST( TestParserComplexRequest , PositiveComplexRequest4){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where (select id from Articles where id=1)=1"));
+}
+
+TEST( TestParserComplexRequest , PositiveComplexRequest5){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles where id=(select id from Articles where id=1)"));
+}
+
+TEST( TestParserCommentary , PositiveCommentary){
+    CALC::CALC_Driver driver;
+    EXPECT_TRUE(driver.parse("select id from Articles -- nimporte quoi après"));
+}
+
+/*
+#######################
+# Ne doit pas marcher #
+#######################
+*/
+
+
+TEST( TestParserSimpleSelect , simpleNegativeSelect1){
+    CALC::CALC_Driver driver;
+    EXPECT_FALSE(driver.parse("SELECT *"));
 }
